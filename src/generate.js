2e3e56d010d6ba3daad88ad0ef65a4dbb115689b
Anton Begunenko <a.d.begun@gmail.com>
2023-09-14 17:19:21 +0200

    add generateTasks button

---
+++ src/generate.js
@@ -0,0 +1,34 @@
+import Docxtemplater from "docxtemplater";
+import PizZip from "pizzip";
+import PizZipUtils from "pizzip/utils/index.js";
+import { saveAs } from "file-saver";
+
+function loadFile(url, callback) {
+  PizZipUtils.getBinaryContent(url, callback);
+}
+
+export const generateTasks = (tasks) => {
+  loadFile(
+    "https://docxtemplater.com/tasks-template.docx",
+    function (error, content) {
+      if (error) {
+        throw error;
+      }
+      const zip = new PizZip(content);
+      const doc = new Docxtemplater(zip, {
+        paragraphLoop: true,
+        linebreaks: true,
+      });
+
+      doc.render({
+       tasks
+      });
+      const blob = doc.getZip().generate({
+        type: "blob",
+        mimeType:
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      }); //Output the document using Data-URI
+      saveAs(blob, "output.docx");
+    }
+  );
+};
